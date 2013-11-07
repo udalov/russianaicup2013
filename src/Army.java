4f9a522961acc30ba42bd4d97dd7d9dd36744970
Alexander Udalov <udalov.alexander@gmail.com>
2013-11-08 02:21:30 +0400

    Prevent infinite loop

--- src/Army.java
+++ src/Army.java
@@ -2,18 +2,25 @@ import model.Direction;
 import model.Trooper;
 import model.World;
 
+import java.util.Arrays;
+
 public class Army {
     private final Point dislocation;
 
     public Army(@NotNull Trooper someTrooper, @NotNull World world) {
-        Board board = new Board(world);
+        this.dislocation = findFreePointNearby(world, new Point(world.getWidth() - someTrooper.getX(), world.getHeight() - someTrooper.getY()));
+    }
 
-        Point p = new Point(world.getWidth() - someTrooper.getX(), world.getHeight() - someTrooper.getY());
-        // TODO: prevent infinite loop
-        while (!board.isPassable(p)) {
-            p = p.go(Direction.SOUTH);
+    @NotNull
+    private Point findFreePointNearby(@NotNull World world, @NotNull Point p) {
+        Board board = new Board(world);
+        for (Direction direction : Util.DIRECTIONS) {
+            for (int it = 0; it < 10; it++) {
+                if (board.isPassable(p)) return p;
+                p = p.go(direction);
+            }
         }
-        this.dislocation = p;
+        throw new IllegalStateException("Impossible as it may seem, there are no free points nearby");
     }
 
     @NotNull
