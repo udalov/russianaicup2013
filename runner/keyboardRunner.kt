f70b5a94f17a7500067cefa724718b41026c602f
Alexander Udalov <udalov.alexander@gmail.com>
2013-11-07 17:32:33 +0400

    API for running battles with any strategies

--- runner/keyboardRunner.kt
+++ runner/keyboardRunner.kt
@@ -7,8 +7,7 @@ import localRunner.*
  */
 public fun main(args: Array<String>): Unit {
     val process = ProcessBuilder("java", "-jar", "lib/local-runner.jar",
-            "true", "true", "3", logFile, "${"-smart" in args.toSet()}", "true")
+            "true", "true", "3", LOG_FILE, "${"-smart" in args.toSet()}", "true")
     process.start()
     runMyStrategy()
-    printLogFile()
 }
--- runner/localRunner.kt
+++ runner/localRunner.kt
@@ -4,39 +4,39 @@ import org.apache.log4j.Logger
 import java.net.ConnectException
 import java.io.File
 
-fun localRunner(vis: Boolean, teamSize: Int, logFile: String, smartGuy: Boolean): Runnable {
-    Logger.getRootLogger()?.removeAllAppenders()
+open class Player(val name: String, val classFile: String)
+object MyStrategy : Player("MyStrategy", "#LocalTestPlayer")
+object EmptyPlayer : Player("EmptyPlayer", javaClass<com.a.b.a.a.e.b>().getSimpleName() + ".class")
+object QuickStartGuy : Player("QuickStartGuy", javaClass<com.a.b.a.a.e.c>().getSimpleName() + ".class")
+object SmartGuy : Player("SmartGuy", javaClass<com.a.b.a.a.e.a>().getSimpleName() + ".class")
 
-    val strategyClass = if (smartGuy) javaClass<com.a.b.a.a.e.a>() else javaClass<com.a.b.a.a.e.c>()
+val LOG_FILE = "out/log.txt"
+
+fun localRunner(vis: Boolean, teamSize: Int, p1: Player, p2: Player, p3: Player, p4: Player): Runnable {
+    Logger.getRootLogger()?.removeAllAppenders()
 
     return com.a.b.c(array(
             "-move-count=50",
             "-render-to-screen=$vis",
             "-render-to-screen-scale=1.0",
             "-render-to-screen-sync=true",
-            "-results-file=" + logFile,
+            "-results-file=$LOG_FILE",
             "-debug=true",
             "-base-adapter-port=31000",
             "-seed=",
-            "-p1-name=EmptyPlayer",
-            "-p2-name=MyStrategy",
-            "-p3-name=" + (if (smartGuy) "SmartGuy 1" else "QuickStartGuy 1"),
-            "-p4-name=" + (if (smartGuy) "SmartGuy 2" else "QuickStartGuy 2"),
-            "-p1-team-size=" + teamSize,
-            "-p2-team-size=" + teamSize,
-            "-p3-team-size=" + teamSize,
-            "-p4-team-size=" + teamSize,
-            javaClass<com.a.b.a.a.e.b>().getSimpleName() + ".class",
-            "#LocalTestPlayer",
-            strategyClass.getSimpleName() + ".class",
-            strategyClass.getSimpleName() + ".class")
-    )
-}
-
-val logFile = "out/log.txt"
-
-fun printLogFile() {
-    println(File(logFile).readText())
+            "-p1-name=${p1.name}",
+            "-p2-name=${p2.name}",
+            "-p3-name=${p3.name}",
+            "-p4-name=${p4.name}",
+            "-p1-team-size=$teamSize",
+            "-p2-team-size=$teamSize",
+            "-p3-team-size=$teamSize",
+            "-p4-team-size=$teamSize",
+            p1.classFile,
+            p2.classFile,
+            p3.classFile,
+            p4.classFile
+    ))
 }
 
 fun runMyStrategy() {
@@ -45,20 +45,23 @@ fun runMyStrategy() {
             Runner.main(array<String>())
         } catch (e: ConnectException) {
             if ("Connection refused" == e.getMessage()) {
-                Thread.sleep(500)
+                Thread.sleep(200)
                 continue
             }
         }
         break
     }
+
+    println(File(LOG_FILE).readText())
 }
 
-/**
- * @param args -vis, -smart
- */
 fun main(args: Array<String>): Unit {
-    val set = args.toSet()
-    Thread(localRunner("-vis" in set, 3, logFile, "-smart" in set)).start()
+    val p1 = QuickStartGuy
+    val p2 = MyStrategy
+    val p3 = QuickStartGuy
+    val p4 = QuickStartGuy
+
+    Thread(localRunner("-vis" in args.toSet(), 3, p1, p2, p3, p4)).start()
+
     runMyStrategy()
-    printLogFile()
 }
