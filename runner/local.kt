7838ca621acef8d085d904298306d0112dd68725
Alexander Udalov <udalov.alexander@gmail.com>
2013-11-24 00:21:40 +0400

    Minor, print current seed

--- runner/local.kt
+++ runner/local.kt
@@ -5,7 +5,10 @@ import java.io.File
 import java.util.Random
 
 fun main(args: Array<String>) = time {
-    runGame("-vis" in args.toSet(), Math.abs(Random().nextLong()), "MSSS")
+    val seed = Math.abs(Random().nextLong())
+    println(seed)
+
+    runGame("-vis" in args.toSet(), seed, "MSSS")
 
     println(File(LOG_FILE).readText())
 }
