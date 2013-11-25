930196315b57032b9d5af6870ad53dc4625c8620
Alexander Udalov <udalov.alexander@gmail.com>
2013-11-25 19:48:43 +0400

    Simplify scripts
    Only places are really needed, points don't matter

--- runner/auto.kt
+++ runner/auto.kt
@@ -2,23 +2,23 @@ package runner.auto
 
 import runner.*
 import java.io.File
-import java.util.ArrayList
 
-fun printShortLog() {
+fun findMyPlace(index: Int): Int {
     val lines = File(LOG_FILE).readLines()
     assert(lines.size() == 6, "Log file should contain exactly 6 lines: $lines")
 
-    val points = ArrayList<Int>(4)
-    for (i in 2..5) {
-        val line = lines[i] split ' '
-        points.add(if (line[2] == "OK") line[1].toInt() else -1000000000)
-    }
-
-    println(points makeString " ")
+    val line = lines[index + 2] split ' '
+    if (line[2] != "OK") return -1
+    return line[0].toInt()
 }
 
 // args = [seed, lineup]
 fun main(args: Array<String>) {
-    runGame(false, args[0].toLong(), args[1])
-    printShortLog()
+    val seed = args[0].toLong()
+    val lineup = args[1]
+    runGame(false, seed, lineup)
+
+    val index = lineup.indexOf('M')
+    assert(index >= 0, "M not found in $lineup")
+    println(findMyPlace(index))
 }
